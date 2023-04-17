1c0c76eef4332657e8ef8684701e0e6ee713ecca
Victor <vitcheffvi@cy-tech.fr>
2023-04-17 19:16:54 +0200

    [transpose.c] finish transpose of 2D matrix

--- gpu_program/transpose.c
+++ gpu_program/transpose.c
@@ -20,24 +20,38 @@ void afficher2DMatrix(int *tableau, int sizeRow, int sizeColumns)
     printf("\n");
 }
 
-
-void init2DMatrix (int *tableau,int sizeRow,int sizeColumns){
+void init2DMatrix(int *tableau, int sizeRow, int sizeColumns)
+{
     int i = 0;
-    for (i = 0; i < sizeRow*sizeColumns; i++){
-        tableau[i]=i;
+    for (i = 0; i < sizeRow * sizeColumns; i++)
+    {
+        tableau[i] = i;
     }
 }
 
-
 void transpose(int *tableau, int *tableauTranspose, int sizeRow, int sizeColumns)
 {
+    int i = 0, j = 0;
+    for (i = 0; i < sizeRow; i++)
+    {
+        for (j = 0; j < sizeColumns; j++)
+        {
+            tableauTranspose[sizeColumns * i + j] = tableau[sizeRow * j + i];
+        }
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    int SIZEROW = 5, SIZECOLUMNS = 10;
+    int SIZEROW = 5, SIZECOLUMNS = 6;
     int *matrix = malloc(SIZEROW * SIZECOLUMNS * sizeof(int));
-    init2DMatrix(matrix,SIZEROW,SIZECOLUMNS);
+    int *transposeMatrix = malloc(SIZECOLUMNS * SIZEROW * sizeof(int));
+    init2DMatrix(matrix, SIZEROW, SIZECOLUMNS);
+    init2DMatrix(transposeMatrix, SIZECOLUMNS, SIZEROW);
     afficher2DMatrix(matrix, SIZEROW, SIZECOLUMNS);
+    transpose(matrix, transposeMatrix, SIZECOLUMNS, SIZEROW);
+    afficher2DMatrix(transposeMatrix, SIZECOLUMNS, SIZEROW);
+    free(matrix);
+    free(transposeMatrix);
     return (0);
 }
