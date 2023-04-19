c95b243022b7d93b1db8a3765b6f386ed65d059d
Victor <vitcheffvi@cy-tech.fr>
2023-04-19 11:54:36 +0200

    add matmul.c

--- gpu_program/multiplication.c
+++ gpu_program/multiplication.c
@@ -28,6 +28,20 @@ void init2DMatrix(int *tableau, int sizeRow, int sizeColumns)
     }
 }
 
+
+
+void matmul(int* mat1, int* mat2, int* out, int dim1, int dim2, int dim_s) {
+    for (int y = 0; y < dim1; y++) {
+        for (int x = 0; x < dim2; x++) {
+            int o = 0;
+            for (int k = 0; k < dim_s; k++) {
+                o += mat1[y * dim_s + k] * mat2[k * dim2 + x];
+            }
+            out[y * dim2 + x] = o;
+        }
+    }
+}
+
 void multiply2Matrix(int *matrice1, int *matrice2, int *produitMatrice, int sizeRow, int sizeColumns)
 {
     int i = 0, j = 0, k = 0;
@@ -38,7 +52,7 @@ void multiply2Matrix(int *matrice1, int *matrice2, int *produitMatrice, int size
             produitMatrice[sizeColumns * i + j] = 0;
             for (k = 0; k < sizeRow; k++)
             {
-                produitMatrice[sizeColumns * i + j] = produitMatrice[sizeColumns * i + j] + matrice1[sizeColumns * i + k] + matrice2[sizeRow * i + k];
+                produitMatrice[sizeColumns * i + j] = produitMatrice[sizeColumns * i + j] + matrice1[sizeColumns * i + k] + matrice2[sizeRow * k + j];
             }
         }
     }
@@ -52,7 +66,10 @@ int main(int argc, char *argv[])
     int *produitMatrix = malloc(SIZEROW1 * SIZECOLUMNS2 * sizeof(int));
     init2DMatrix(matrix1, SIZEROW1, SIZECOLUMNS1);
     init2DMatrix(matrix2, SIZEROW2, SIZECOLUMNS2);
-    multiply2Matrix(matrix1, matrix2, produitMatrix, SIZEROW1, SIZECOLUMNS2);
+    /*multiply2Matrix(matrix1, matrix2, produitMatrix, SIZEROW1, SIZECOLUMNS2);*/
+    afficher2DMatrix(matrix1, SIZEROW1, SIZECOLUMNS2);
+    afficher2DMatrix(matrix2, SIZEROW1, SIZECOLUMNS2);
+    matmul(matrix1,matrix2,produitMatrix,SIZEROW1,SIZECOLUMNS2,SIZECOLUMNS1);
     afficher2DMatrix(produitMatrix, SIZEROW1, SIZECOLUMNS2);
     free(matrix1);
     free(matrix2);
